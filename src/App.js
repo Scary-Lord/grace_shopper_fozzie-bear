6c67057fabce15f52bc4991a3269836ae639f092
delvis Soto <delvissoto99@icloud.com>
2023-02-28 14:45:40 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Route, Routes } from "react-router-dom";
-import { Contact, Help } from "./components/Footer";
+import {Route, Routes } from "react-router-dom";
+import Contact from "./components/Footer/Footer";
+import Help from "./components/Footer/Footer";
 
 function App() {
   return (
