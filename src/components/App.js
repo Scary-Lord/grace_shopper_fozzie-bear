73f60b3b66b12884dc716f6b8c76c4754f988db6
hearnmp <117321628+hearnmp@users.noreply.github.com>
2023-02-28 16:38:47 -0500

    fixed imports for footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import {Route, Routes } from "react-router-dom";
-import Contact from "./components/Footer/Footer";
-import Help from "./components/Footer/Footer";
+import { Contact, Help } from "../components/Footer";
 
 function App() {
   return (
