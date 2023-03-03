ead759815a2f66d4e48b61c46041169828f1f560
jzelo <jonzeloof@gmail.com>
2023-03-03 16:59:02 -0500

    more css stuff and fixed the homepage rendering

--- src/components/Homepage/categoryboxes.js
+++ src/components/Homepage/categoryboxes.js
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom';
 const CategoryBoxes = () => {
     return (
         <div className='catagory-boxes'>
-            <Link to ="/products/monitors">
+            <Link to ="/products/monitors" className="category-box">
                 <h3>Monitors</h3>
             </Link>
-            <Link to ="/products/keyboards">
+            <Link to ="/products/keyboards" className="category-box">
                 <h3>Keyboards</h3>
             </Link>
-            <Link to ="/products/mouses">
+            <Link to ="/products/mouses" className="category-box">
                 <h3>Mouses</h3>
             </Link>
-            <Link to ="/products/accessories">
+            <Link to ="/products/accessories" className="category-box">
                 <h3>Accessories</h3>
             </Link>
         </div>
     )
 };
+
 
 export default CategoryBoxes;
