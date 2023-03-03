b70825528cc20d4431c92805ecf89d26eab89e58
jzelo <jonzeloof@gmail.com>
2023-03-03 17:07:18 -0500

    blah

--- src/components/Homepage/categoryboxes.js
+++ src/components/Homepage/categoryboxes.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const CategoryBoxes = () => {
     return (
-        <div className='catagory-boxes'>
+        <div className='category-boxes'>
             <Link to ="/products/monitors" className="category-box">
                 <h3>Monitors</h3>
             </Link>
